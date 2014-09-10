e7086429d635573f8fcfb0b08aa3cedb4ac78569
Josh Justice <josh@need-bee.com>
2014-09-10 06:54:23 -0400

    hides list of dependencies

--- app/views/packages/index.blade.php
+++ app/views/packages/index.blade.php
@@ -6,17 +6,12 @@
             <h2>Packages</h2>
                 @foreach( $packageUsage as $package )
                     <div class="package">
-                        <div class="row">
-                            <div class="medium-10 columns">
-                                <h3><a href="{{{ $package->homepage }}}">{{{ $package->name }}}</a></h3>
-                                <p>{{{ $package->description }}}</p>
-                            </div>
-                            <div class="medium-2 columns">
-                                <div><button class="usages button small">&#9733; {{{ count($package->usages) }}}</button></div>
-                            </div>
+                        <div>
+                            <button class="usages button small right">&#9733; {{{ count((array)$package->usages) }}}</button>
+                            <h3><a href="{{{ $package->homepage }}}">{{{ $package->name }}}</a></h3>
+                            <p>{{{ $package->description }}}</p>
                         </div>
                         <div class="usages" style="display:none">
-                            <p>Used in {{{ pluralize($package->usages, 'app') }}}:</p>
                             <ul>
                                 @foreach( $package->usages as $usage => $version )
                                     <li>{{{ $usage }}}: {{{ $version }}}</li>
@@ -28,4 +23,18 @@
             </ul>
         </div>
     </div>
+@stop
+
+@section('js')
+<script type="text/javascript">
+
+require(['jquery'], function($) {
+    $(function(){
+        $('button.usages').click(function(evt){
+            $(this).closest('.package').find('div.usages').toggle();
+        });
+    });
+});
+
+</script>
 @stop
