1cfb6810161bd57f55ac5d2b7cf567e29503caa9
Josh Justice <josh@need-bee.com>
2014-09-10 06:43:45 -0400

    begins styling layout

--- app/views/packages/index.blade.php
+++ app/views/packages/index.blade.php
@@ -4,18 +4,26 @@
     <div class="row">
         <div class="columns">
             <h2>Packages</h2>
-            <ul>
                 @foreach( $packageUsage as $package )
-                    <li>
-                        <h3><a href="{{{ $package->homepage }}}">{{{ $package->name }}}</a><h3>
-                        <p>{{{ $package->description }}}</p>
-                        <p>Used in {{{ pluralize($package->usages, 'app') }}}:</p>
-                        <ul>
-                            @foreach( $package->usages as $usage => $version )
-                                <li>{{{ $usage }}}: {{{ $version }}}</li>
-                            @endforeach
-                        </ul>
-                    </li>
+                    <div class="package">
+                        <div class="row">
+                            <div class="medium-10 columns">
+                                <h3><a href="{{{ $package->homepage }}}">{{{ $package->name }}}</a></h3>
+                                <p>{{{ $package->description }}}</p>
+                            </div>
+                            <div class="medium-2 columns">
+                                <div><button class="usages button small">&#9733; {{{ count($package->usages) }}}</button></div>
+                            </div>
+                        </div>
+                        <div class="usages" style="display:none">
+                            <p>Used in {{{ pluralize($package->usages, 'app') }}}:</p>
+                            <ul>
+                                @foreach( $package->usages as $usage => $version )
+                                    <li>{{{ $usage }}}: {{{ $version }}}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
                 @endforeach
             </ul>
         </div>
