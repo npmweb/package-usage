5093ce8d7be8343c839e120a40a16216df1f19b5
Josh Justice <josh@need-bee.com>
2014-09-11 06:12:32 -0400

    fixes title spacing

--- app/views/layouts/_headtag.blade.php
+++ app/views/layouts/_headtag.blade.php
@@ -1,6 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1, user-scalable=0" />
 
-<title>PackageUsage</title>
+<title>Package Usage</title>
 
 <link rel="stylesheet" type="text/css" href="{{ asset('includes/shared/components/foundation/css/foundation.min.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('includes/css/main.css') }}" />
